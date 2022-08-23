5cd226b0fd018056f0db9349e6c3a5ecb2f8af82
frelon <jokemetoday@outlook.com>
2022-08-23 23:15:18 +0800

    点单完成92%

--- src/components/orderOptionp.vue
+++ src/components/orderOptionp.vue
@@ -1,6 +1,14 @@
 <template>
 	<div>
-
+		<div class="outb">
+			<div class="smbx">
+				仅可以选择取消订单!不可以删除订单信息.
+				<br>
+				并且需要选择原因.默认是[顾客主动退款]
+				<br>
+				如果奶茶已经做出,且无质量原因客观上不接受取消订单退款.
+			</div>
+		</div>
 	</div>
 </template>
 
