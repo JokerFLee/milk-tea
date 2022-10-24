7a1e0a8d8ba419ccbcbb2fd31a2b6302b9fc1a23
frelon <jokemetoday@outlook.com>
2022-10-25 01:41:26 +0800

    继续完成没有完成的代码

--- src/components/milkteaDIY.vue
+++ src/components/milkteaDIY.vue
@@ -13,8 +13,8 @@
 						</div>
 
 						<div class="butt">
-							<button>修改</button>
-							<button>查看</button>
+							<button @click="modifythis(item.guid)">修改</button>
+							<button>默认</button>
 						</div>
 
 					</div>
@@ -25,22 +25,90 @@
 	</div>
 
 
-	<div class="mask">
+	<div class="mask" v-show="mask">
 		<div class="mask_main">
 			<div class="mask_card comm">
 
 				<div class="mask_card_item">
-					dlfhgkdfug
+					<div class="smell it-c">
+						<h2>味道</h2>
+						<input placeholder="例如：七分甜 五分甜" @focusout="updateDate(0)" @keyup="updateDate(0)" v-model="diyParams[0]">
+					</div>
+					<div class="temperature it-c">
+						<h2>温度</h2>
+						<input placeholder="例如：常温 冰镇 去冰" @focusout="updateDate(1)" @keyup="updateDate(1)" v-model="diyParams[1]">
+					</div>
+					<div class="content it-c">
+						<h2>加料</h2>
+						<input placeholder="例如：加芋泥 加葡萄" @focusout="updateDate(2)" @keyup="updateDate(2)" v-model="diyParams[2]">
+					</div>
+
+					<div class="other it-c">
+						<h2>其他</h2>
+						<input placeholder="例如：风味" v-model="diyParams[3]">
+						<input placeholder="例如： Florence Москва" @focusout="updateDate(4)" @keyup="updateDate(4)"
+							v-model="diyParams[4]">
+					</div>
+
+					<span class="note">备注：内容用空格隔开，为空就不显示</span>
+
 				</div>
 
 				<div class="mask_card_button">
-					hosidkfhksduufgh
+					<button @click="setDefault">默认设置</button>
+					<button @click="printll">提交</button>
+					<button @click="cancel">取消</button>
 				</div>
 
 			</div>
 
-			<div class="massk_preview comm">
+			<div class="mask_preview comm">
+				<div class="maskbox">
+					<div class="head">
+						<h2>Preview</h2>
+					</div>
+					<div class="body">
+						<div class="container">
 
+							<div class="comment" v-show="diyParams[0]">
+								<div class="title">
+									<h2>味道</h2>
+								</div>
+								<div class="content">
+									<span v-for="it in diyParamsList[0]">{{it}}</span>
+								</div>
+							</div>
+
+							<div class="comment" v-show="diyParams[1]">
+								<div class="title">
+									<h2>温度</h2>
+								</div>
+								<div class="content">
+									<span v-for="it in diyParamsList[1]">{{ it }} </span>
+								</div>  
+							</div>
+
+							<div class="comment" v-show="diyParams[2]">
+								<div class="title">
+									<h2>加料</h2>
+								</div>
+								<div class="content">
+									<span v-for="it in diyParamsList[2]">{{ it }}</span>
+								</div>
+							</div>
+
+							<div class="comment" v-show="diyParams[3] || diyParams[4]">
+								<div class="title">
+									<h2>{{diyParams[3]}}</h2>
+								</div>
+								<div class="content">
+									<span v-for="it in diyParamsList[3]">{{it}}</span>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
 			</div>
 
 		</div>
@@ -54,11 +122,57 @@ import { onMounted, ref } from "vue";
 import { getDescMilkteaList } from "../utils/milktee/axgetamilktea"
 
 let main_milktlist = ref([])
+let mask = ref(false)
+let guid = ""
+let diyParams = ref([])
+let diyParamsList = ref([[],[],[],[]])
+
+function printll() {
+	console.log(diyParamsList.value);
+}
+
+function modifythis(e){
+	guid = e
+	mask.value = true
+}
+
+function updateDate(e) {
+	switch (e) {
+		case 0:
+			diyParamsList.value[0] = diyParams.value[0].split(' ')
+			break;
+		case 1:
+			diyParamsList.value[1] = diyParams.value[1].split(' ')
+			break
+		case 2:
+			diyParamsList.value[2] = diyParams.value[2].split(' ')
+			break
+		case 4:
+			diyParamsList.value[3] = diyParams.value[4].split(' ')
+			break
+	}
+
+}
+
+function setDefault(){
+	let smell = "七分甜 五分甜 三分甜 不甜"
+	let temperature = "加热 常温 冰镇"
+	let food = "椰果 珍珠 西柚 芋泥 米麻薯 冰粉 晶球"
+	diyParams.value[0] = smell
+	diyParams.value[1] = temperature
+	diyParams.value[2] = food
+	updateDate(0)
+	updateDate(1)
+	updateDate(2)
+}
+
+function cancel(){
+	mask.value = false
+}
 
 onMounted(() => {
 	getDescMilkteaList().then((e) => {
 		main_milktlist.value = e
-		console.log(e);
 	})
 })
 </script>
@@ -84,6 +198,7 @@ onMounted(() => {
 			min-height: 95%;
 			height: 95%;
 			padding: 5px 0;
+			
 
 			.card_box {
 				width: auto;
@@ -92,16 +207,18 @@ onMounted(() => {
 				justify-content: center;
 				flex-wrap: wrap;
 				padding-bottom: 2.5%;
+				
 
 				.card_item {
 					height: auto;
 					min-height: 150px;
 					width: 45%;
-					background-color: #ddd;
-					margin: 2px 5px;
-					border-radius: 7px;
+					background-color: #fff;
+					margin: 4px 5px;
+					border-radius: 8px;
 					overflow: hidden;
 					position: relative;
+					box-shadow: 2px 2px 3px #ddd,-2px -2px 3px #eee;
 
 					.info {
 						width: 40%;
@@ -112,7 +229,10 @@ onMounted(() => {
 						align-items: center;
 						flex-direction: column;
 						backdrop-filter: blur(10px);
-						-webkit-backdrop-filter:blur(10px);
+						-webkit-backdrop-filter: blur(10px);
+						border-top-left-radius: 8px;
+						border-bottom-left-radius: 8px;
+
 						span {
 							width: 80%;
 						}
@@ -151,10 +271,14 @@ onMounted(() => {
 							height: 35px;
 							border-radius: 15px;
 							border: none;
-							background-color: #ffffffbf;
+							background-color: rgba(255, 255, 255, 0);
+							backdrop-filter: blur(10px);
+							-webkit-backdrop-filter: blur(10px);
+							box-shadow: 2px 2px 3px #ccc,-2px -2px 3px #eee;
 							color: #444;
 							font-weight: bold;
 							cursor: pointer;
+							margin-right: 3px;
 
 						}
 
@@ -196,26 +320,192 @@ onMounted(() => {
 		overflow: auto;
 
 		.comm {
-			min-width: 350px;
-			width: 90%;
 			height: 90%;
+			min-width: 350px;
 			margin: 5px 15px;
-			flex: 1;
+			display: flex;
+			flex-direction: column;
+			border-radius: 17px;
+
 		}
 
 		.mask_card {
-			background-color: bisque;
+			background-color: #eee;
+			
+			width: 60%;
+			.mask_card_item {
+				width: 100%;
+				height: 80%;
+				display: flex;
+				align-items: center;
+				justify-content: space-evenly;
+				flex-direction: column;
 
-			.mask_card_item {}
+				.it-c {
+					width: 80%;
+					height: 30%;
+					display: flex;
+					align-items: center;
+					justify-content: space-evenly;
+					flex-direction: column;
+				}
+
+				// .smell {}
+				// .content {}
+				// .temperature {}
+				.other {
+					display: flex;
+					flex-direction: column;
+
+					input {
+						margin-top: 2px;
+					}
+				}
+
+				.note {
+					margin-top: 10px;
+					font-size: small;
+					color: rgb(186, 186, 186);
+				}
+
+				h2 {
+					margin: 0;
+				}
+
+				::placeholder {
+					color: #bbb;
+				}
+
+				input {
+					// width: 200px;
+					width: 80%;
+					height: 25px;
+					line-height: 25px;
+					border-color: #dbdbdb;
+					border-radius: 9px;
+					padding-left: 15px;
+					border-color: rgba(0, 0, 0, 0);
+					// font-weight: bold;
+				}
+
+				input:focus {
+					border-color: #582aee;
+					outline: none;
+				}
+
+				input:focus::placeholder {
+					color: #777;
+				}
+			}
 
 
-			.mask_card_button {}
+			.mask_card_button {
+				width: 100%;
+				height: 20%;
+				display: flex;
+				align-items: center;
+				justify-content: space-evenly;
 
+				button {
+					height: 40px;
+					width: 30%;
+					border-radius: 8px;
+					border: none;
+					background: #ddd;
+					color: #111;
+					font-size: 15px;
+				}
+
+				button:hover:hover {
+					background: #582aee;
+					color: #fff;
+				}
+			}
 		}
 
-		.massk_preview {
-			background-color: seagreen;
+		.mask_preview {
+			background-color: #eee;
+			user-select: none;
+			width: 30%;
 
+			.maskbox {
+				width: 100%;
+				height: 100%;
+				user-select: none;
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				
+
+				h2 {
+					margin: 0;
+				}
+
+				.head {
+					width: 100%;
+					height: 5%;
+					margin: 10px 0;
+
+					h2 {
+						text-align: center;
+					}
+				}
+
+				.body {
+					width: 90%;
+					height: 90%;
+					border-radius: 8px;
+					box-shadow:2px 2px 3px #ccc,-2px -2px 3px #fff;
+
+
+					.container {
+						width: 100%;
+						height: 100%;
+						display: flex;
+						flex-direction: column;
+						align-items: center;
+						justify-content: space-evenly;
+
+						.comment {
+							width: 80%;
+							height: 20%;
+							display: flex;
+							flex-direction: column;
+
+							.title {
+								h2 {
+									text-align: center;
+								}
+							}
+
+							.content {
+								width: 100%;
+								height: 100%;
+								display: flex;
+								justify-content: space-evenly;
+								margin-top: 5px;
+								span{
+									width: 80px;
+									height: 40px;
+									margin: 0 2px;
+									display: inline-flex;
+									border-radius: 7px;
+									color: #222;
+									align-items: center;
+									justify-content: center;
+									padding: 2px;
+									cursor: pointer;
+									box-shadow:2px 2px 3px #bbb,-2px -2px 3px #fff;
+								}
+								span:hover{
+										background: #582aee;
+										color: #fff;
+									}
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 
